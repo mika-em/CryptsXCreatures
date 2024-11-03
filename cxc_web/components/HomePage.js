b0b193e5d0b39ae23236218e3d2dde674b488b5e
mikaem <mkmanaligod@gmail.com>
2024-11-03 14:08:55 -0800

    Refactor HomePage to dynamically render buttons based on user login status

--- cxc_web/components/HomePage.js
+++ cxc_web/components/HomePage.js
@@ -1,4 +1,4 @@
-import { navigateTo, isLoggedIn, logout } from "../app.js";
+import { navigateTo, checkLoginStatus, logout } from "../app.js";
 
 export default function HomePage() {
   const container = document.createElement("div");
@@ -7,34 +7,38 @@ export default function HomePage() {
   const header = document.createElement("h1");
   header.classList.add("display-4");
 
-  const userEmail = localStorage.getItem("userEmail");
-  header.textContent =
-    isLoggedIn() && userEmail
-      ? `Hello, ${userEmail}!`
-      : "Welcome to Crypts X Creatures!";
-
   const loginButton = document.createElement("button");
-  loginButton.classList.add("btn", "btn-primary", "me-2");
+  loginButton.classList.add("btn", "btn-primary", "me-2", "mt-3");
   loginButton.textContent = "Login";
   loginButton.onclick = () => navigateTo("/login");
 
   const registerButton = document.createElement("button");
-  registerButton.classList.add("btn", "btn-secondary");
+  registerButton.classList.add("btn", "btn-secondary", "mt-3");
   registerButton.textContent = "Register";
   registerButton.onclick = () => navigateTo("/register");
 
   const logoutButton = document.createElement("button");
   logoutButton.classList.add("btn", "btn-danger");
   logoutButton.textContent = "Logout";
-  logoutButton.onclick = logout;
-
-  container.appendChild(header);
-  if (isLoggedIn()) {
-    container.appendChild(logoutButton);
-  } else {
-    container.appendChild(loginButton);
-    container.appendChild(registerButton);
+  logoutButton.onclick = () => {
+    logout();
+    renderButtons(false);
   }
 
+  async function renderButtons(isLoggedIn) {
+    const email = localStorage.getItem("userEmail");
+    header.textContent = isLoggedIn && email ? `Hello, ${email}!`   : "Welcome to Crypts x Creatures!"
+    
+    container.innerHTML = "";
+    container.appendChild(header);
+
+    if (isLoggedIn()) {
+      container.appendChild(logoutButton);
+    } else {
+      container.appendChild(loginButton);
+      container.appendChild(registerButton);
+    }
+  }
+  checkLoginStatus().then(renderButtons);
   return container;
 }
