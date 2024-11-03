3ce30195dec02162445a49a0e62e2f9890ddfc2b
Cheryl Lau <clau136@my.bcit.ca>
2024-11-03 13:50:02 -0800

    Adding logout

--- cxc_api/server.js
+++ cxc_api/server.js
@@ -92,6 +92,11 @@ router.get('/admin/users', verifyJWT, checkAdminRole, async (req, res) => {
   }
 });
 
+router.post('/logout', (res) => {
+  Utils.invalidateCookie(res, 'token');
+  res.status(200).send('Logged out successfully.');
+});
+
 
 router.get('/forgotpassword', async (req, res) => {
   const { email } = req.query;
