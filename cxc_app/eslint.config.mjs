18a9d9052bac6d624631c1cb660112f6585735bf
mikaem <mkmanaligod@gmail.com>
2024-11-18 12:58:50 -0800

    fix eslint

---
+++ cxc_app/eslint.config.mjs
@@ -0,0 +1,26 @@
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintConfigNext from 'eslint-config-next';
+
+export default [
+    {
+        files: ['**/*.js', '**/*.jsx'],
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            ...eslintConfigNext.rules,
+            'prettier/prettier': 'error',
+        },
+        extends: [eslintConfigNext, eslintConfigPrettier],
+    },
+];
